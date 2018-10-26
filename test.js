084f517bb7874e2e145621521156868eb728132e
GanievStas <benony0gmail.com>
2018-10-27 08:42:37 +1000

    Функции

--- test.js
+++ test.js
@@ -1,16 +1,33 @@
 "use strict";
 
-var a = +prompt("a?", "");
-
-switch (a) {
-    case 0:
-        alert("0");
-        break;
-    case 1:
-        alert("1");
-        break;
-    case 2:
-    case 3:
-        alert("2,3");
-        break;
+function checkAge (age) {
+    return age > 18 || confirm('Родители разрешили?');
 }
+
+function checkAge1 (age) {
+    return (age > 18)? true : confirm('Родители разрешили?');
+}
+
+function min (a, b) {
+    return (a < b)? a : b;
+}
+
+function pow (x, n) {
+
+    var res = 1;
+
+    for(var i = 1; i <= n; i++) {
+        res *= x;
+    }
+
+    return res;
+
+}
+
+var px, pn, pres;
+
+px = prompt('Основание:', 0);
+pn = prompt('Степень:', pn);
+pres = pow(px, pn);
+
+alert('Результат: ' + pres);

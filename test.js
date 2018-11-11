06f8c26980ae6a87fe1699d308ed8b4e1ba445bc
GanievStas <benony0gmail.com>
2018-11-12 06:50:04 +1000

    Работа с числами

--- test.js
+++ test.js
@@ -64,5 +64,50 @@ function fib(n) {
 
 }
 
-var pn = +prompt('Число:', 0);
-alert('Результат: ' + fib(pn));
+/*
+    функции работы с числами
+*/
+
+function stepOfRound() {
+    return 10;
+}
+
+function isNumeric(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+function sum(n1, n2) {
+
+    var res;
+
+    if(!isNumeric(n1) || !isNumeric(n2)) {
+        return NaN;
+    }
+
+    res = +n1 + +n2;
+
+    return res.toFixed(stepOfRound());
+
+}
+
+function randomToFloat(n) {
+    return Math.random() * n;
+}
+
+function randomFloatInterval(n1, n2) {
+    return n1 + Math.random() * (n2 - n1);
+}
+
+function randomInt(n1, n2) {
+
+    var min = Math.floor(n1);
+    var max = Math.round(n2) + 1;
+
+    return Math.round(min + Math.random() * (max - min));
+
+}
+
+var n1 = prompt('Число 1:', 0);
+var n2 = prompt('Число 2:', 0);
+
+alert(sum(n1, n2));
